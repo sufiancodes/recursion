0480a70537aaeee7631d880761d0ea81cf282cbb
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-04-19 20:41:52 +0500

    add functionality to loop and sum
    functionality add to loop and sum over the first and second number and push it in to array

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,3 +1,12 @@
 def fibonacci_iteratively
-  
+  number = gets.chomp.to_i
+  first_number = 0
+  second_number = 1
+  result = []
+  for i in 1..number
+    sum = first_number + second_number
+    result.push(sum)
+  end
 end
+
+fibonacci_iteratively()
