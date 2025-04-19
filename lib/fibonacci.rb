784c6b36cb72bea647cd8c0001e427cbfe424a0c
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-04-19 20:59:22 +0500

    Issue regarding missing first two number resolve
    the first two number were missing from fibonacci sequence issue resolved by adding first two already in the array

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -2,8 +2,8 @@ def fibonacci_iteratively
   number = gets.chomp.to_i
   first_number = 0
   second_number = 1
-  result = []
-  for i in 1..number
+  result = [0,1]
+  for i in 1..number-2
     sum = first_number + second_number
     result.push(sum)
     first_number = second_number
