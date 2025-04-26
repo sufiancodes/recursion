cb32b0205b7e30eee2e92849d5a20b07feb925d8
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-04-26 17:00:45 +0500

    Add logic for divide
    logic add to split array in two half left and right

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,3 +1,11 @@
-def merge_sort(high,low)
+def merge_sort(array)
+  mid = array.length/2
+  left_half = array[0...mid]
+  right_half = array[mid..-1]
   
 end
+
+# array = [1,2,3,4,5,6]
+# mid = array.length/2
+# left_half = array[0...mid]
+# right_half = array[mid..-1]
