b623625544dc0d98ec2e4692c7f92640003da5d0
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-04-27 17:39:23 +0500

    Add method for finding mid
    method for finding mid is create to make code more readable and manageable

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,11 +1,14 @@
 def merge_sort(array)
+  find_mid(array)
+  
+end
+def find_mid(array)
   mid = array.length/2
   left_half = array[0...mid]
   right_half = array[mid..-1]
-  
 end
-
 # array = [1,2,3,4,5,6]
 # mid = array.length/2
 # left_half = array[0...mid]
 # right_half = array[mid..-1]
+# p right_half
