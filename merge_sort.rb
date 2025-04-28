322f996445d3e41f6724bed8680639f87941b457
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-04-28 22:20:08 +0500

    Add method to merge two half
    method to add the two half add which takes left and right half and merge them correctly

--- merge_sort.rb
+++ merge_sort.rb
@@ -9,3 +9,15 @@ def merge_sort(array)
   merge_sort(right_half)
 
 end
+
+def merge_two_halves(first_half, second_half)
+  final_array = []
+  while first_half.any && second_half.any
+    if first_half[0] < second_half[0]
+      final_array << first_half[0].shift
+    else
+      final_array << second_half[0].shift
+    end
+  end
+  return final_array
+end
