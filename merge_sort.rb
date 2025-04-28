3332b57552a4e8b6c86e9b1bf4c9a5006630d12e
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-04-28 22:30:09 +0500

    Merge sort complete
    all the logic required for merge sort add array sorted with merge sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -5,14 +5,14 @@ def merge_sort(array)
   mid = array.length/2
   left_half = array[0...mid]
   right_half = array[mid..-1]
-  merge_sort(left_half)
-  merge_sort(right_half)
-
+  left_side = merge_sort(left_half)
+  right_side = merge_sort(right_half)
+  merge_two_halves(left_side,right_side)
 end
 
 def merge_two_halves(first_half, second_half)
   final_array = []
-  while first_half.any && second_half.any
+  while first_half.any? && second_half.any?
     if first_half[0] < second_half[0]
       final_array << first_half.shift
     else
@@ -23,3 +23,6 @@ def merge_two_halves(first_half, second_half)
   final_array += second_half
   return final_array
 end
+
+array = [1,2,34,55,9,22]
+p merge_sort(array)
