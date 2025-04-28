c9a51303168904d04463f90fd66fe038e7318b24
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-04-28 22:24:49 +0500

    Fix logic for merge method
    method fix to correctly add all element to the final array and produce good result

--- merge_sort.rb
+++ merge_sort.rb
@@ -14,10 +14,12 @@ def merge_two_halves(first_half, second_half)
   final_array = []
   while first_half.any && second_half.any
     if first_half[0] < second_half[0]
-      final_array << first_half[0].shift
+      final_array << first_half.shift
     else
-      final_array << second_half[0].shift
+      final_array << second_half.shift
     end
   end
+  final_array += first_half
+  final_array += second_half
   return final_array
 end
